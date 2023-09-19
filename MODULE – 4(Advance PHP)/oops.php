70a2a71f103c55e33e581bb35e9e0c4c88f277f0
kasamalimukhi <kasamalimukhi003@gmail.com>
2023-09-20 00:58:31 +0530

    Module – 4(Advance PHP)

---
+++ MODULE – 4(Advance PHP)/oops.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Advance php</title>
+</head>
+
+<body>
+    <strong><u>OOPS</u></strong><br>
+    <li>What Is Object Oriented Programming?<br>
+        <b>Ans :</b><br>
+        Object-oriented programming has a sweeping impact because it appeals at multiple levels and promises faster and
+        cheaper development and maintenance. It follows a bottom-up approach to develop applications. <br>
+        The word object-oriented is the combination of two words i.e. object and oriented. The dictionary meaning of the
+        object is an article or entity that exists in the real world. The meaning of oriented is interested in a
+        particular kind of thing or entity. In layman's terms, it is a programming pattern that rounds around an object
+        or entity are called object-oriented programming. <br>
+        It is the most popular programming model among developers. It is well suited for programs that are large,
+        complex, and actively updated or maintained. It simplifies software development and maintenance by providing
+        major concepts such as abstraction, inheritance, polymorphism, and encapsulation. These core concepts support
+        OOP.<br>
+
+        A real-world example of OOP is the automobile. It more completely illustrates the power of object-oriented
+        design.<br>
+
+    </li>
+    <li>
+    What Are Properties Of Object Oriented Systems?<br>
+    <b>Ans :</b><br>
+    
+    </li>
+</body>
+
+</html>
